224a14d117dba1d8364a1730d0222f9361eb2c30
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-11-08 22:46:12 +0900

    feat: MemberRepository commit

--- src/main/java/jpaBook/jpaShop/domain/member/MemberRepository.java
+++ src/main/java/jpaBook/jpaShop/domain/member/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -16,7 +17,21 @@ public class MemberRepository {
         return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
+        /*단건 조회 첫 파라미터에 타입, 두번 째에 pk 값을 넣어주면 된다.*/
         return entityManager.find(Member.class, id);
     }
+
+    public List<Member> findAll() {
+                                        /*table이 아닌 객체에서 찾는다는 점*/
+        return entityManager.createQuery("select m from Meber m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return entityManager.createQuery("select m from Member m where m.nam = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
 }
