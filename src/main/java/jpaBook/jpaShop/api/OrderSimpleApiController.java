0178cd7cc00355d5ede1f7d70082b8bccfacfefa
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-11-25 23:36:12 +0900

    feat: OrderSimpleApiController.java commit

---
+++ src/main/java/jpaBook/jpaShop/api/OrderSimpleApiController.java
@@ -0,0 +1,23 @@
+package jpaBook.jpaShop.api;
+
+import jpaBook.jpaShop.domain.order.Order;
+import jpaBook.jpaShop.domain.order.OrderRepository;
+import jpaBook.jpaShop.domain.order.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
