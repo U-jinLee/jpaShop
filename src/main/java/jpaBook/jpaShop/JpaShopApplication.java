1f95a844dac0a8dd25d3801c3328f048be1153e0
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-11-26 00:00:14 +0900

    feat: OrderSimpleApiController.java commit
    - 엔티티를 직접 노출해서 값을 반환하는 법(권장되지 않음)

--- src/main/java/jpaBook/jpaShop/JpaShopApplication.java
+++ src/main/java/jpaBook/jpaShop/JpaShopApplication.java
@@ -1,7 +1,9 @@
 package jpaBook.jpaShop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaShopApplication {
@@ -10,4 +12,8 @@ public class JpaShopApplication {
 		SpringApplication.run(JpaShopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
+	}
 }
